e9eac98d8f8d0c6d5170ca47718d3405d9272ab5
FerretCode <49322226+FerretCode@users.noreply.github.com>
2021-04-26 17:50:02 -0700

    update readme to include new promise changes, as well as test promises

--- tests.js
+++ tests.js
@@ -1,4 +1,6 @@
 const dog = require(__dirname + '/index.js');
 
 //dog.JSONPushKey('test.json', 'testKey4', 'testKey1.testKey2.testKey3');
-dog.JSONPushValue('test.json', 'testkey1', 'testvalue2', 'testvalue2', 'testkey1');
+dog.JSONPushValue('test.json', 'testkey1', 'testvalue2', 'testvalue2', 'testkey1').then((v) => {
+	console.log(v);
+});
